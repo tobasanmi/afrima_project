79a553aee5069a7e7690eeee238c3cbb7fef38ae
unknown <tobasanmi@gmail.com>
2022-11-18 15:03:49 +0000

    navbar design

---
+++ src/navBar/Navbar.js
@@ -0,0 +1,20 @@
+
+function Navbar() {
+  return (
+    <div className="navBarContainer">
+      <div className="logo">Logo</div>
+      <div className="linksWrapper">
+        <div className="about">About us</div>
+        <div className="events">Events</div>
+        <div className="faq">FAQ</div>
+        <div className="sponsors">Sponsors & Exhibitors </div>
+      </div>
+      <div className="input">
+        <input></input>
+        <div>My Afrima</div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
