ef755ef8071208a4ee4bc562cba60750f676669e
unknown <tobasanmi@gmail.com>
2022-11-25 15:10:21 +0000

    created the landing page

--- src/Pages/landingPage.js
+++ src/Pages/landingPage.js
@@ -2,6 +2,29 @@ import React from 'react'
 
 export default function LandingPage() {
   return (
-    <div>landingPage</div>
-  )
+    <div className="landinpageWrapper">
+      <div classname="landingPage">
+      <div className="iconWrapper">
+        <div>pipeTop</div>
+        <ul>
+          <li>twitter</li>
+          <li>fb</li>
+          <li>instagram</li>
+          <li>linkedIn</li>
+          <li>Youtube</li>
+        </ul>
+        <div>PipeBottom</div>
+      </div>
+      <div className="wordingsWrapper">
+        <h1>
+          CREATIVE AFRICA NEXUS (CAREX) <span>WKND</span>
+          25-27 NOVEMBER 2022
+        </h1>
+        <h3>Ivory coast</h3>
+        <a href="#home">Register Now</a>
+      </div>
+      </div>
+      
+    </div>
+  );
 }
