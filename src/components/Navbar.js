2684112c54ef6b10a38883f2d852d8eb3d08753f
unknown <tobasanmi@gmail.com>
2022-11-26 23:18:00 -0800

    adjusted the navbar link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,16 @@
+import React from "react";
+import {Link} from 'react-router-dom'
+
 function Navbar() {
   return (
     <div className="navBarContainer">
       <div className="logoWrapper">
         <h1 className="logo">Logo</h1>
         <ul className="linksWrapper">
-          <li className="about">About us</li>
-          <li className="events">Events</li>
-          <li className="faq">FAQ</li>
-          <li className="sponsors">Sponsors & Exhibitors</li>
+          <li><Link to ='/' className="about">About us</Link></li>
+          <li><Link to = '/events' className="events">Events</Link></li>
+          <li><Link to= "/faq" className="faq">FAQ</Link></li>
+          <li><Link to ='/sponsors' className="sponsors">Sponsors & Exhibitors</Link></li>
         </ul>
       </div>
       <form className="form">
